87cc3ef108ef594f2a05aff6a4841a831f5e50b4
annuc1s <annuciis121@gmail.com>
2020-09-03 19:40:16 +0000

    adding parameters to be passed from contact form to EmailJs service

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+//contact form linked to private email using EmailJs
+//allows for the passing of parameters from contact form to the EmailJS service
+function sendMail(contactForm) {
+    emailjs.send("gmail", "template_o4rvgv4", {
+        "fname": contactForm.firstname.value,
+        "lname": contactForm.lastname.value,
+        "email": contactForm.email.value,
+        "subject": contactForm.subject.value,
+        "message": contactForm.message.value,
+    })
+}
