387fae718e379dd1b3e2b0a039acde88a860886b
annuc1s <annuciis121@gmail.com>
2020-09-03 19:42:52 +0000

    response and error functions added to sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -8,4 +8,13 @@ function sendMail(contactForm) {
         "subject": contactForm.subject.value,
         "message": contactForm.message.value,
     })
+     .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("ERROR", error);
+            }
+        );
+    return false;
 }
