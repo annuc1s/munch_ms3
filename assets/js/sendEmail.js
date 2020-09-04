b0c267b5215dcc3077eddb5966867ac32a48058b
annuc1s <annuciis121@gmail.com>
2020-09-04 09:34:49 +0000

    added variables and function to clear the form after submission

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,4 +1,10 @@
 //contact form linked to private email using EmailJs
+//variables that are used to clear the form after submission see below function(response)
+let firstNameInput = document.getElementById("fname");
+let secondNameInput = document.getElementById("lname");
+let emailInput = document.getElementById("email");
+let subjectInput = document.getElementById("subject");
+let messageInput = document.getElementById("message");
 //allows for the passing of parameters from contact form to the EmailJS service
 function sendMail(contactForm) {
     emailjs.send("gmail", "template_o4rvgv4", {
@@ -8,8 +14,15 @@ function sendMail(contactForm) {
         "subject": contactForm.subject.value,
         "message": contactForm.message.value,
     })
+    //responses after the submission of form: SUCCESS / ERROR
      .then(
             function (response) {
+    //values that clear the contact form after successful submission of form
+                firstNameInput.value = '';
+                secondNameInput.value = '';
+                emailInput.value = '';
+                subjectInput.value = '';
+                messageInput.value = '';
                 console.log("SUCCESS", response);
             },
             function (error) {
