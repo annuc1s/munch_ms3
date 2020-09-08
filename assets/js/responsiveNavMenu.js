03ec7585aeec2af55aa118fc7fbc0ab65d493b3f
annuc1s <annuciis121@gmail.com>
2020-09-08 20:29:10 +0000

    responsive nav bar script moved to assets/js/responsiveNavMenu.js

---
+++ assets/js/responsiveNavMenu.js
@@ -0,0 +1,9 @@
+//Mobile Navigation Menu function
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
